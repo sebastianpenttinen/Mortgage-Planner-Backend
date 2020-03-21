0dc5b21b1195a446d5e7be45773e46144bc85a12
sebastianpenttinen <sebastian.penttinen@abo.fi>
2020-03-21 19:01:01 +0200

    added Swagger

--- src/main/java/MortagePlan/app/AppApplication.java
+++ src/main/java/MortagePlan/app/AppApplication.java
@@ -3,7 +3,10 @@ package MortagePlan.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan(basePackages={"MortagePlan.app"})
 public class AppApplication {
 	public static void main(String[] args) {
